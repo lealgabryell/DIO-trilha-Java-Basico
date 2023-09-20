788aefaef39d768bfa972ec761b97d3d473c5c91
gabryell <g.lealrocha3@gmail.com>
2023-09-20 15:32:17 -0300

    conta com filtragem po data

---
+++ ContaBancariaDesafio/ContaBancaria/ContaBanco/src/Conta.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Conta {
+
+  public static void main(String[] args) throws ParseException {
+    try (Scanner scanner = new Scanner(System.in)) {
+        String dataInicial = scanner.nextLine();
+        String dataFinal = scanner.nextLine();
+
+        SistemaAcionistas sistemaAcionistas = new SistemaAcionistas();
+        List < String > analises = sistemaAcionistas.obterAnalisesDesempenho(dataInicial, dataFinal);
+
+        for (String analise: analises) {
+          System.out.println(analise);
+        }
+    }
+  }
+}
+
+class SistemaAcionistas {
+  public List < String > obterAnalisesDesempenho(String dataInicialStr, String dataFinalStr) throws ParseException {
+    SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    Date dataInicial = df.parse(dataInicialStr);
+    Date dataFinal = df.parse(dataFinalStr);
+
+    // Simula uma base da dados em uma lista de análises:
+    List < Analise > analises = new ArrayList < > ();
+    analises.add(new Analise(df.parse("01/01/2023"), "Analise de Desempenho Financeiro"));
+    analises.add(new Analise(df.parse("15/02/2023"), "Analise de Riscos e Exposicoes"));
+    analises.add(new Analise(df.parse("31/03/2023"), "Analises Corporativas"));
+    analises.add(new Analise(df.parse("01/04/2023"), "Analise de Politicas e Regulamentacoes"));
+    analises.add(new Analise(df.parse("15/05/2023"), "Analise de Ativos"));
+    analises.add(new Analise(df.parse("30/06/2023"), "Analise de Inovacao e Tecnologia"));
+    
+    List < String > analiseFiltrada = new ArrayList<> ();
+    for (Analise i: analises){
+        if (i.data.compareTo(dataInicial)>=0 && i.data.compareTo(dataFinal) <=0){
+            analiseFiltrada.add(i.descricao);
+        }
+    }
+    return analiseFiltrada;
+  }
+
+  class Analise {
+    Date data;
+    String descricao;
+
+    public Analise(Date data, String descricao) {
+      this.data = data;
+      this.descricao = descricao;
+    }
+  }
+}
--- Desafio ControleFluxo/Contador/src/Contador.java
+++ Desafio ControleFluxo/Contador/src/Contador.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -7,21 +8,20 @@ public class Contador {
         System.out.println("Digite o segundo parametro: ");
         int p2 = sc.nextInt();
 
-        try{
+        try {
             contar(p1, p2);
-        } 
-        catch(ParametrosInvalidosException e){
+        } catch (ParametrosInvalidosException e) {
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
 
     }
 
-    static void contar(int p1, int p2) throws ParametrosInvalidosException{
-        if(p1 > p2){
+    static void contar(int p1, int p2) throws ParametrosInvalidosException {
+        if (p1 > p2) {
             throw new ParametrosInvalidosException();
         }
         int contagem = p2 - p1;
-        for (int i = 1; i<= contagem; i++) {
+        for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
         }
     }
